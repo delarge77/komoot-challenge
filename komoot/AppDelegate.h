69dec59b96f5ba68f161de2521a49d4d691badfb
Alessandro dos santos pinto <lastripper@gmail.com>
2016-01-10 12:18:19 -0200

    Initial Commit

---
+++ komoot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  komoot
+//
+//  Created by Alessandro dos santos pinto on 1/10/16.
+//  Copyright © 2016 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ komoot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  komoot
+//
+//  Created by Alessandro dos santos pinto on 1/10/16.
+//  Copyright © 2016 Alessandro dos Santos Pinto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
